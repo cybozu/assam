df841fed375f7459a740f679ace5eb3e97b8e3a6
jumpei-miyata <jumpei-miyata@cybozu.co.jp>
2019-03-28 17:20:58 +0900

    Configure options from prompt
    Co-authored-by: Jumpei Miyata <jumpei-miyata@cybozu.co.jp>
    Co-authored-by: Hideki Igarashi <hideki.develop@gmail.com>

--- main.go
+++ main.go
@@ -1,49 +1,7 @@
 package main
 
-import (
-	"context"
-	"github.com/cybozu/arws/aws"
-	"github.com/cybozu/arws/idp"
-	"log"
-)
+import "github.com/cybozu/arws/cmd"
 
 func main() {
-	cfg, err := NewConfig()
-	if err != nil {
-		log.Panic(err)
-	}
-
-	request, err := aws.CreateSAMLRequest(cfg.AppIDURI)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	azure := idp.NewAzure(request, cfg.AzureTenantID)
-	base64Response, err := azure.Authenticate(ctx)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	response, err := aws.ParseSAMLResponse(base64Response)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	roleArn, principalArn, err := aws.ExtractRoleArnAndPrincipalArn(*response)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	credentials, err := aws.AssumeRoleWithSAML(ctx, roleArn, principalArn, base64Response)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	err = aws.SaveCredentials("default", *credentials)
-	if err != nil {
-		log.Panic(err)
-	}
+	cmd.Execute()
 }
---
+++ prompt/prompt.go
@@ -0,0 +1,132 @@
+package prompt
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Prompt provides CLI prompt functionality
+type Prompt struct {
+	writer  io.Writer
+	scanner *bufio.Scanner
+}
+
+// Options is option for Prompt
+type Options struct {
+	Default      string
+	ValidateFunc ValidateFunc
+}
+
+// ValidateFunc is function to validate input value
+type ValidateFunc func(string) error
+
+// NewPrompt returns Prompt struct
+func NewPrompt() Prompt {
+	return Prompt{
+		writer:  os.Stdout,
+		scanner: bufio.NewScanner(os.Stdin),
+	}
+}
+
+// AskString asks query and returns input string
+func (p *Prompt) AskString(query string, options *Options) (string, error) {
+	if options == nil {
+		options = &Options{}
+	}
+
+	for {
+		// prompt
+		err := p.printPrompt(query, options)
+		if err != nil {
+			return "", err
+		}
+
+		// scan
+		val, err := p.scanString()
+		if err != nil {
+			return "", err
+		}
+		if val == "" {
+			return options.Default, nil
+		}
+
+		// validate
+		if options.ValidateFunc != nil {
+			err := options.ValidateFunc(val)
+			if err != nil {
+				_, err := fmt.Fprintln(p.writer, err.Error())
+				if err != nil {
+					return "", err
+				}
+				continue
+			}
+		}
+
+		return val, nil
+	}
+}
+
+// AskInt asks query and returns input int
+func (p *Prompt) AskInt(query string, options *Options) (int, error) {
+	if options == nil {
+		options = &Options{}
+	}
+
+	for {
+		// prompt
+		err := p.printPrompt(query, options)
+		if err != nil {
+			return 0, err
+		}
+
+		// scan
+		val, err := p.scanString()
+		if err != nil {
+			return 0, err
+		}
+		if val == "" {
+			return strconv.Atoi(options.Default)
+		}
+
+		// validate
+		if options.ValidateFunc != nil {
+			err := options.ValidateFunc(val)
+			if err != nil {
+				_, err := fmt.Fprintln(p.writer, err.Error())
+				if err != nil {
+					return 0, err
+				}
+				continue
+			}
+		}
+
+		return strconv.Atoi(val)
+	}
+}
+
+func (p *Prompt) printPrompt(query string, options *Options) error {
+	if options.Default != "" {
+		_, err := fmt.Fprintf(p.writer, "%s (Default: %s): ", query, options.Default)
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := fmt.Fprintf(p.writer, "%s: ", query)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *Prompt) scanString() (string, error) {
+	if !p.scanner.Scan() {
+		return "", p.scanner.Err()
+	}
+
+	return strings.TrimSpace(p.scanner.Text()), nil
+}
